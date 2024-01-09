0375ef7958d95f3a295a22ec1fb8fe9c1e8b3c90
Oscar Espinoza <oscaranto.espinoza@gmail.com>
2024-01-09 17:26:47 -0300

    Fix: Search by params

--- src/UI/TodoSearch/index.js
+++ src/UI/TodoSearch/index.js
@@ -1,15 +1,22 @@
-import "./TodoSearch.css";
+import { useSearchParams } from 'react-router-dom';
+import './TodoSearch.css';
 
-function TodoSearch({ searchValue, setSearchValue, loading }) {
+function TodoSearch({ setSearchValue, loading }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const paramsValue = searchParams.get('search');
+
+  const onSearchValueChange = ({ target: { value } }) => {
+    setSearchValue(value);
+    setSearchParams({ search: value });
+  };
+
   return (
     <input
       className="TodoSearch"
       type="text"
       placeholder="Cortar cebolla"
-      value={searchValue}
-      onChange={(e) => {
-        setSearchValue(e.target.value);
-      }}
+      value={paramsValue || ''}
+      onChange={onSearchValueChange}
       disabled={loading}
     />
   );
